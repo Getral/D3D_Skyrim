f8136b1aa1c409614b28d57615dd286a1836825f
Luidingo <110881809+Luidingo@users.noreply.github.com>
2024-03-04 14:47:20 +0900

    Debug Branch

--- base/DirectX3D/Objects/Character/Player.h
+++ base/DirectX3D/Objects/Character/Player.h
@@ -5,12 +5,12 @@ class Player : public ModelAnimator
 public:
 	struct Status
 	{
-		float curHp = 50;
+		float curHp = 100;
 		float maxHp = 100;
 		float curstamina = 100;
 		float maxstamina = 100;
 		float atk = 10;
-		float speed = 50;
+		float speed = 10;
 	};
 
 private:
