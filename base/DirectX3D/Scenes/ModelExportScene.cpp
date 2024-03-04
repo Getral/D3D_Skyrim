b613ad211b810c1d3439267dbe82b96904f177dd
Luidingo <110881809+Luidingo@users.noreply.github.com>
2024-03-04 14:36:56 +0900

    Debug Branch

--- base/DirectX3D/Scenes/ModelExportScene.cpp
+++ base/DirectX3D/Scenes/ModelExportScene.cpp
@@ -3,7 +3,7 @@
 
 ModelExportScene::ModelExportScene()
 {
-    string name = "shield";
+    string name = "alduin";
     string file = "Models/FBX/" + name + ".fbx";
     ModelExporter* exporter;
     string clipName;
