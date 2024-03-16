b29493194e75eefcf6017ad5ea005ed64a10cc4e
Luidingo <boboss4253@gmail.com>
2024-03-16 20:15:42 +0900

    지형 파일 수정

--- base/DirectX3D/Scenes/TerrainScene.cpp
+++ base/DirectX3D/Scenes/TerrainScene.cpp
@@ -6,14 +6,29 @@ TerrainScene::TerrainScene()
 	for (int i = 0; i < height; i++)
 	{
 		vector<Terrain*> tmp;
-		for (int j = 0; j < width; i++)
+		for (int j = 0; j < width; j++)
 		{
-			wstring file = L"Textures/HeightMaps/Tamriel/TamrielBeta_10_2016_01_" + to_wstring(/*j + i * 16 + */1) + L".gif";
+			wstring index = L"";
+			if (j + i * 32 + 1 < 10)
+				index = L"00";
+			else if (j + i * 32 + 1 < 100)
+				index = L"0";
+			index += to_wstring(j + i * 32 + 1);
+			wstring file = L"Textures/HeightMaps/Tamriel/TamrielBeta_10_2016_01-" + index + L".png";
 			tmp.push_back(new Terrain(L"Textures/Landscape/tamriel.4.0.0.dds", file));
-			tmp[j]->Pos() = { -(tmp[j]->GetSize().x * (50 - j)) + tmp[j]->GetSize().x * 0.5f , 0,
-				-(tmp[j]->GetSize().y * (50 - j)) + tmp[j]->GetSize().y * 0.5f };
 		}
 		terrain.push_back(tmp);
+	}
+
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			terrain[i][j]->Pos().SetX(-(terrain[i][j]->GetSize().x * (width / 2 - j)) + terrain[i][j]->GetSize().x * 0.5f);
+			terrain[i][j]->Pos().SetY(-100);
+			terrain[i][j]->Pos().SetZ(-(terrain[i][j]->GetSize().y * (height / 2 - i)) + terrain[i][j]->GetSize().y * 0.5f);
+			terrain[i][j]->UpdateWorld();
+		}
 	}
 }
 
@@ -26,6 +41,9 @@ TerrainScene::~TerrainScene()
 
 void TerrainScene::Update()
 {
+	for (vector<Terrain*> terrains : terrain)
+		for (Terrain* t : terrains)
+			t->UpdateWorld();
 }
 
 void TerrainScene::PreRender()
