2c4928412797db101f4f490457ff60dfe7bbae2e
minsoo1254 <minsoo1254@naver.com>
2024-03-20 21:42:17 +0900

    여러건물가능

--- base/DirectX3D/Scenes/ModelExportScene.cpp
+++ base/DirectX3D/Scenes/ModelExportScene.cpp
@@ -3,7 +3,7 @@
 
 ModelExportScene::ModelExportScene()
 {
-    string name = "male_dragonbone";
+    string name = "rock_large1";
     string file = "Models/FBX/" + name + ".fbx";
     ModelExporter* exporter;
     string clipName;
--- base/DirectX3D/Scenes/PlayerScene.cpp
+++ base/DirectX3D/Scenes/PlayerScene.cpp
@@ -8,13 +8,16 @@ PlayerScene::PlayerScene()
 	aldu = new alduin();
 	aldu->SetTarget(player);
 
-	CAM->SetTarget(player); 
-	CAM->TargetOptionLoad("Skyrim2");
-	CAM->LookAtTarget(); 
+	//CAM->SetTarget(player); 
+	//CAM->TargetOptionLoad("Skyrim2");
+	//CAM->LookAtTarget(); 
 
-	ObjectManager::Get()->Create({ 50,0,-50 }, "farmhouse");
-	ObjectManager::Get()->Create({ 0,0,0 }, "farmhouse2");
+	ObjectManager::Get()->Create({ 0,0,0 }, "farmhouse");
+	ObjectManager::Get()->Create({ 0,50,0 }, "farmhouse");
+	ObjectManager::Get()->Create({ 0,100,0 }, "farmhouse");
+	ObjectManager::Get()->Create({ 0,150,0 }, "farmhouse");
 	ObjectManager::Get()->Create({ -50,0,50 }, "ebonydagger");
+	ObjectManager::Get()->Create({ -50,50,50 }, "ebonydagger");
 }
 
 PlayerScene::~PlayerScene()
@@ -37,7 +40,7 @@ void PlayerScene::PreRender()
 
 void PlayerScene::Render()
 {
-	player->Render();
+	//player->Render();
 	//aldu->Render();
 	ObjectManager::Get()->Render();
 }
@@ -53,5 +56,5 @@ void PlayerScene::GUIRender()
 	//player->GUIRender();
 	//aldu->GUIRender();
 
-	//ObjectManager::Get()->GUIRender();
+	ObjectManager::Get()->GUIRender();
 }
